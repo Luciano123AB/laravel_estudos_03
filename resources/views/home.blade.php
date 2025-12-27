7ca92317d0a392167e338b8050e471474980fe18
Luciano Eduardo <luciano123ab@gmail.com>
2025-12-27 19:33:49 -0300

    002(ImportandoRecursos_Layout)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,11 @@
 @section("page_title", "Home Page")
     
 @section("content")
-    <p>Conteúdo da minha view.</p>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col card p-5 bg-secondary text-center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet est reprehenderit quae hic nobis accusantium saepe, ut ad, corporis excepturi cum veniam facere labore. Magni dolorem iste dolore repellat! Quae.
+            </div>
+        </div>
+    </div>
 @endsection
