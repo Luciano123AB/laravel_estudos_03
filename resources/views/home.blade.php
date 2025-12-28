de16132ec6bcfc19653d20b4cd4e2f486a143061
Luciano Eduardo <luciano123ab@gmail.com>
2025-12-27 22:10:59 -0300

    004(PassandoDados_BladeComponents)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,11 @@
 @section("content")
     <h3>Este texto faz parta da view.</h3>
     {{-- Renderizar o component: --}}
-    <x-my-component />
+    <x-my-component message="Mensagem passada para dentro do componente." />
 
     {{-- Renderizar component que existe dentro de subpasta. --}}
-    <x-admin.admin-card />
+    <x-admin.admin-card :name="$myName" teste="Olá Mundo!" />
     <h3>Este texto faz parte da view.</h3>
+
+    <p>{{ $myName }}</p>
 @endsection
