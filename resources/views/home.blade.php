f3800e3f901dc2cb5d84b4430b5ad825d98a8bfe
Luciano Eduardo <luciano123ab@gmail.com>
2025-12-30 20:01:29 -0300

    008(Introducao_Slots)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,17 @@
 @extends("layouts.main_layout")
 
 @section("content")
-    <div class="text-center">
+    {{-- <div class="text-center">
         @foreach($pessoas_linguas as $pessoa => $linguas)
             <x-card-pessoa :$pessoa :$linguas />
         @endforeach
+    </div> --}}
+
+    {{-- Componentes e Slots --}}
+    <div>
+        <h4 class="text-info">Como funciona um Slot?</h4>
+        <x-other-card>
+            <h1 class="text-danger">Este é um slot!</h1>
+        </x-other-card>
     </div>
 @endsection
