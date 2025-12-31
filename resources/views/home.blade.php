5a6bbd30d123335f8cd34e5fbf65ea8fc20de555
Luciano Eduardo <luciano123ab@gmail.com>
2025-12-30 21:55:34 -0300

    009(Componentes_VariosSlots)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,22 @@
     </div> --}}
 
     {{-- Componentes e Slots --}}
-    <div>
+    {{-- <div>
         <h4 class="text-info">Como funciona um Slot?</h4>
         <x-other-card>
             <h1 class="text-danger">Este é um slot!</h1>
         </x-other-card>
-    </div>
+    </div> --}}
+
+    <x-multi_slot>
+        <x-slot:title>Este é o título.</x-slot>
+        <x-slot:content>Este é o conteúdo.</x-slot>
+        <x-slot:footer>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+            </ul>
+        </x-slot>
+    </x-multi_slot>
 @endsection
