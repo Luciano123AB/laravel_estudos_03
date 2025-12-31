91aa0e199c8464fe5f30f45711bb30a3c9b7daf4
Luciano Eduardo <luciano123ab@gmail.com>
2025-12-30 22:18:07 -0300

    011(BladeLayouts_Componentes)

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,5 @@
+<x-component-layout>
+    <x-slot:title>CONTACT</x-slot>
+
+    <h1>CONTACTS</h1>
+</x-component-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,9 @@
-@extends("layouts.main_layout")
+<x-component-layout>
+    <x-slot:title>HOME</x-slot>
 
-@section("content")
-    {{-- <div class="text-center">
-        @foreach($pessoas_linguas as $pessoa => $linguas)
-            <x-card-pessoa :$pessoa :$linguas />
-        @endforeach
-    </div> --}}
-
-    {{-- Componentes e Slots --}}
-    {{-- <div>
-        <h4 class="text-info">Como funciona um Slot?</h4>
-        <x-other-card>
-            <h1 class="text-danger">Este é um slot!</h1>
-        </x-other-card>
-    </div> --}}
-
-    {{-- <x-multi_slot>
-        <x-slot:title>Este é o título.</x-slot>
-        <x-slot:content>Este é o conteúdo.</x-slot>
-        <x-slot:footer>
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-            </ul>
-        </x-slot>
-    </x-multi_slot> --}}
-
-    <h4>Componente Anônimo</h4>
-    <x-alert-card>Primeira mensagem.</x-alert-card>
-    <x-alert-card>Segunda mensagem.</x-alert-card>
-    <x-alert-card>Terceira mensagem.</x-alert-card>
-@endsection
+    <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+    </ul>
+</x-component-layout>
